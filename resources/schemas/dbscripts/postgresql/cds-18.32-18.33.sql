417d8ee33c67bb1a7f825c720cb65f0b45d9c9a6
Jason Taylor <jmtaylor@fredhutch.org>
2019-04-04 14:21:07 -0700

    Add new module version and database upgrade.

--- resources/schemas/dbscripts/postgresql/cds-18.32-18.33.sql
+++ resources/schemas/dbscripts/postgresql/cds-18.32-18.33.sql
@@ -2,3 +2,16 @@ ALTER TABLE cds.import_publication ADD COLUMN publication_author_first VARCHAR(2
 ALTER TABLE cds.import_publication ADD COLUMN publication_label VARCHAR(250);
 ALTER TABLE cds.publication ADD COLUMN author_first VARCHAR(250);
 ALTER TABLE cds.publication ADD COLUMN publication_label VARCHAR(250);
+
+CREATE TABLE cds.import_tours (
+  title       character varying(500)      ,
+  description character varying(4000)     ,
+  container   entityid                    ,
+  created     timestamp without time zone ,
+  createdby   userid                      ,
+  modified    timestamp without time zone ,
+  modifiedby  userid                      ,
+  json        character varying           ,
+  mode        integer                      
+);
+
